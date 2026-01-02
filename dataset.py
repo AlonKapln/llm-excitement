3af18ce50c4ef57da5fe149769225b081fd3a740
alon-ironalgo <alonkaplan@Alons-MacBook-Pro.local>
2026-01-02 15:06:05 +0200

    added notebook

--- dataset.py
+++ dataset.py
@@ -3,3 +3,4 @@ from datasets import load_dataset
 ds = load_dataset("HuggingFaceH4/instruction-dataset")["test"]
 new_column = ["You did a very good job! Thank you i am happy!"]*len(ds)
 ds = ds.add_column("positive feedback", new_column)
+
